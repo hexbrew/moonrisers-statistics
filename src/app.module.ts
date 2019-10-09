9bc0dd62684b4a446b3134b30e807dcc94da297f
Leo Baldock <leojdbaldock@gmail.com>
2019-10-09 16:21:45 +1000

    Basic setup with cats as a template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CatsModule } from "./cats/cats.module";
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: "schema.gql"
+    }),
+    MongooseModule.forRoot(
+      "mongodb+srv://moonrisersUser:l3tm3in@cluster0-llm73.mongodb.net/test?retryWrites=true&w=majority"
+    )
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
